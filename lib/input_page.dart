32b6f59fce40323797746117b6c9d2013192f555
Sadaqat Panhwer <panhwersadaqat22@gmail.com>
2023-04-03 22:15:57 +0400

    init commit

---
+++ lib/input_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class InputPage extends StatefulWidget {
+  @override
+  _InputPageState createState() => _InputPageState();
+}
+
+class _InputPageState extends State<InputPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('BMI CALCULATOR'),
+      ),
+      body: Container(
+        color: const Color(0xFF1D1E33),
+      )
+    );
+  }
+}
