cc5f074abc2a7cf94015f8c081bbba4fa38f06cc
Sadaqat Panhwer <panhwersadaqat22@gmail.com>
2023-04-05 23:37:01 +0400

    worked on UI

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
 
+const bottomContainerHeight = 80.0;
+const cardActiveColor = Color(0xFF1D1E33);
+const bottomContainerColor = Color(0xFFEB1555);
+
+
 class InputPage extends StatefulWidget {
   @override
   _InputPageState createState() => _InputPageState();
@@ -12,9 +17,69 @@ class _InputPageState extends State<InputPage> {
       appBar: AppBar(
         title: const Text('BMI CALCULATOR'),
       ),
-      body: Container(
-        color: const Color(0xFF1D1E33),
+      body: Column(
+        children:  <Widget>[
+          Expanded(child: Row(
+            children:  <Widget>[
+              Expanded(
+                  child: ReusableCard(
+                   cardActiveColor,
+                  )
+              ),
+              Expanded
+                (child: ReusableCard(
+                 cardActiveColor
+              )
+              )
+            ],
+          )),
+            Expanded(
+              child: ReusableCard(
+                 cardActiveColor
+              )
+          ),
+          Expanded(child: Row(
+            children: <Widget> [
+              Expanded(
+                  child: ReusableCard(
+                      cardActiveColor
+                  )
+              ),
+              Expanded(
+                  child: ReusableCard(
+                     cardActiveColor
+                  )
+              )
+            ],
+          )),
+          Container(
+            color: bottomContainerColor,
+            margin: const EdgeInsets.only(top: 10.0),
+            width: double.infinity,
+            height: bottomContainerHeight,
+          )
+        ],
       )
+    );
+  }
+}
+
+class ReusableCard extends StatelessWidget {
+
+
+  ReusableCard(this.color,[this.childCard]);
+
+  final Color color;
+  Widget? childCard;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.all(15.0),
+      decoration:  BoxDecoration(
+        color:  color,
+        borderRadius: BorderRadius.circular(10.0),
+      ),
     );
   }
 }
